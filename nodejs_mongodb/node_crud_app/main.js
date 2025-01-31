5ffc6637a539977c1e47fd20dab602d285f70f8c
DialloOumarou <157978549+DialloOumarou@users.noreply.github.com>
2025-01-31 17:30:59 +0000

    project setup complete

--- nodejs_mongodb/node_crud_app/main.js
+++ nodejs_mongodb/node_crud_app/main.js
@@ -7,9 +7,33 @@ const session = require('express-session');
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.get('/',(req,res)=>{
-    res.send('hello world');
+//database connection
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', (error)=>console.log(error));
+db.once('open', ()=>console.log('Connected to the database'));
+
+//middlewares
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(session({
+    secret:'my_secret_key',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use((req,res,next)=>{
+    res.locals.message = req.session.message;
+    delete req.session.message;
+    next();
 });
+
+//set template engine
+app.set('view engine', 'ejs');
+
+//routes prefixes
+app.use("",require('./routes/routes'));
+
 
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
---
+++ nodejs_mongodb/node_crud_app/models/Users.js
@@ -0,0 +1,27 @@
+const { name } = require('ejs');
+const mongoose = require('mongoose');
+
+const usersSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Users', usersSchema);
