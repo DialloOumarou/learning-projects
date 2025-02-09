b3092f299002a2eee0cb43f432a78c159a7c15cf
DialloOumarou <157978549+DialloOumarou@users.noreply.github.com>
2025-02-09 10:18:53 +0000

    first commit of 09 fev

--- MERN_project/MERN_product_store/frontend/src/main.jsx
+++ MERN_project/MERN_product_store/frontend/src/main.jsx
@@ -1,12 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
   <StrictMode>
-    <Provider>
-      <App />
-    </Provider>
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
