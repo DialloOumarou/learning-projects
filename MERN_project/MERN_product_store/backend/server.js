90b07f2c648ee1f40d3016b146f9a3d85b65f828
DialloOumarou <157978549+DialloOumarou@users.noreply.github.com>
2025-02-12 17:51:25 +0000

    last ajustement on the backend

--- MERN_project/MERN_product_store/backend/server.js
+++ MERN_project/MERN_product_store/backend/server.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import { connectDb } from './config/db.js';
 import Product from './models/product.model.js';
 import Products from './models/product.model.js';
+import mongoose from 'mongoose';
+import productRoutes from "./route/product.route.js"
 
 dotenv.config();
 
@@ -10,53 +12,10 @@ const app = express();
 
 app.use(express.json());//allows us to accept json data in the body of the request
 
+app.use("/api/products",productRoutes);
 
 
-app.get('/api/products', async (req,res)=>{
-    try{
-        const products = await Product.find({});
-        res.status(200).json({success:true,data:products});
-    }
-    catch(error){
-        console.log("error in fetching the product: ",error.message); 
-        res.status(500).json({success:false,message:"server error"});
-    }
-});
-
-
-app.post('/api/products', async (req, res) => {
-    const product = req.body;
-     
-    if(!product.name || !product.price || !product.image){
-
-        return res.status(400).json({sucess:false,message:'please provide all fields'});
-    }
-
-    const newProduct = new Product(product);
-
-    try{
-        await newProduct.save();
-        res.status(201).json({success:true,data:newProduct});
-    }
-    catch(error){
-        console.log("error in creating product error:",error.message);
-        res.status(500).json({success:false,message:'internal server error'});
-    }
-    
-});
-
-
-app.delete('/api/products/:id',async (req,res)=>{
-    const {id} = req.params;
-    try{
-        await Product.findByIdAndDelete(id);
-        res.status(200).json({success:true,message:'product deleted'});
-    }
-    catch(error){
-        console.log("error in deleting products");
-        res.status(404).json({success:false,message:"product not found"});
-    }
-});
+const PORT = process.env.PORT || 5000;
 
 
 
@@ -64,10 +23,9 @@ app.delete('/api/products/:id',async (req,res)=>{
 
 
 
-
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDb();
-    console.log('Server is running on port 5000');
+    console.log('Server is running on port: '+PORT);
 });
 
 
